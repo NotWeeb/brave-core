2ab5f5f7490eac15407fb77d94cc3472c890f869
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2020-04-03 12:15:03 -0400

    wire DisconnectWallet call

--- android/java/org/chromium/chrome/browser/BraveRewardsNativeWorker.java
+++ android/java/org/chromium/chrome/browser/BraveRewardsNativeWorker.java
@@ -403,6 +403,12 @@ public class BraveRewardsNativeWorker {
         }
     }
 
+    public void DisconnectWallet(String wallet_type) {
+        synchronized (lock) {
+            nativeDisconnectWallet(mNativeBraveRewardsNativeWorker, wallet_type);
+        }
+    }
+
     @CalledByNative
     public void OnGetRewardsMainEnabled(boolean enabled) {
         int oldRewardsStatus = rewardsStatus;
@@ -573,6 +579,13 @@ public class BraveRewardsNativeWorker {
         }
     }
 
+    @CalledByNative
+    public void OnDisconnectWallet(int error_code, String external_wallet) {
+        for (BraveRewardsObserver observer : mObservers) {
+            observer.OnDisconnectWallet(error_code, external_wallet);
+        }
+    }
+
     private native void nativeInit();
     private native void nativeDestroy(long nativeBraveRewardsNativeWorker);
     private native void nativeCreateWallet(long nativeBraveRewardsNativeWorker);
@@ -617,4 +630,5 @@ public class BraveRewardsNativeWorker {
     private native void nativeSetAdsPerHour(long nativeBraveRewardsNativeWorker, int value);
     private native boolean nativeIsAnonWallet(long nativeBraveRewardsNativeWorker);
     private native void nativeGetExternalWallet(long nativeBraveRewardsNativeWorker, String wallet_type);
+    private native void nativeDisconnectWallet(long nativeBraveRewardsNativeWorker, String wallet_type);
 }
